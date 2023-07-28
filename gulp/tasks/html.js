30e863187f9fc5f31317eba89edf3f6134ee5f24
Alex Isakov <86313135+aleksei-isakov@users.noreply.github.com>
2023-07-28 16:04:06 +0300

    Add files via upload

---
+++ gulp/tasks/html.js
@@ -0,0 +1,44 @@
+import gulp from 'gulp';
+import fileInclude from 'gulp-file-include';
+import webpHtml from 'gulp-webp-html-nosvg';
+import versionNumber from 'gulp-version-number';
+import htmlMin from 'gulp-htmlmin';
+
+import { plugins } from '../config/plugins.js';
+import { filePaths } from '../config/paths.js';
+import { isBuild } from '../../gulpfile.js';
+
+const html = () => {
+  return gulp
+    .src(filePaths.src.html)
+    .pipe(plugins.handleError('HTML'))
+    .pipe(fileInclude())
+    .pipe(plugins.replace(/@img\//g, 'images/'))
+    .pipe(
+      htmlMin({
+        useShortDoctype: true,
+        sortClassName: true,
+        removeComments: isBuild,
+
+        /** Раскомментировать если требуется минификация html */
+        //collapseWhitespace: isBuild,
+      })
+    )
+    .pipe(
+      plugins.if(
+        isBuild,
+        versionNumber({
+          value: '%DT%',
+          append: {
+            key: '_v',
+            cover: 0,
+            to: ['css', 'js'],
+          },
+        })
+      )
+    )
+    .pipe(gulp.dest(filePaths.build.html))
+    .pipe(plugins.browserSync.stream());
+};
+
+export { html };
